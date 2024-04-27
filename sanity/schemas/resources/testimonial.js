2450fe602f481bfa74f3cf4666485f4e79e71997
jxhnkndl <jkroyston@gmail.com>
2024-04-27 10:36:46 -0400

    add sort order and hidden fields to testimonials model

--- sanity/schemas/resources/testimonial.js
+++ sanity/schemas/resources/testimonial.js
@@ -30,8 +30,26 @@ const testimonial = {
       description: 'Provide the body of the testimonial',
       validation: (rule) =>
         rule.required().warning('🤖 You must include the testimonial body'),
+    },
+    {
+      name: 'order',
+      title: 'Sort Order',
+      type: 'number',
+      description: 'In what order should the testimonial appear in the slider? NOTE: Leaving this field blank will place the testimonial at the end of the testimonials slider'
+    },
+    {
+      name: 'hidden',
+      title: 'Hidden',
+      type: 'boolean',
+      initialValue: false,
+      description: 'Flip this switch to hide the testimonial from public view'
     }
-  ]
+  ],
+  preview: {
+    select: {
+      title: 'name',
+    }
+  }
 };
 
 export default testimonial;
