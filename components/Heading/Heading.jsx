40a323120b05c9ab9b103a1f7b342dda8058ced7
jxhnkndl <jkroyston@gmail.com>
2024-05-01 16:27:58 -0400

    create reusable subheading component

--- components/Heading/Heading.jsx
+++ components/Heading/Heading.jsx
@@ -6,14 +6,6 @@ const Heading = ({ level, children }) => {
       return <h1 className={`${styles.heading}`}>{children}</h1>;
     case 'h2':
       return <h2 className={`${styles.heading}`}>{children}</h2>;
-    case 'h3':
-      return <h3 className={`${styles.heading}`}>{children}</h3>;
-    case 'h4':
-      return <h4 className={`${styles.heading}`}>{children}</h4>;
-    case 'h5':
-      return <h5 className={`${styles.heading}`}>{children}</h5>;
-    case 'h6':
-      return <h6 className={`${styles.heading}`}>{children}</h6>;
   }
 };
 
