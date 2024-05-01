d367b9406f44b2fdff4d75b3dd483880abe23b95
jxhnkndl <jkroyston@gmail.com>
2024-05-01 11:05:09 -0400

    adjust brand block height on landscape mobile breakpoints

--- components/BrandStatement/BrandStatement.jsx
+++ components/BrandStatement/BrandStatement.jsx
@@ -6,24 +6,21 @@ const BrandStatement = ({ color, heading, image, textOrder, imageOrder }) => {
   return (
     <div className={`${styles.grid}`}>
       <div className={`${styles.textBlock}`}>
-        {color === 'primary' && (
-          <div className={`${styles.textContainer} ${styles.primary}`}>
-            <h2 className={`${styles.heading} brandStatement`}>{heading}</h2>
-          </div>
-        )}
-        {color === 'secondary' && (
-          <div className={`${styles.textContainer} ${styles.secondary}`}>
-            <h2 className={`${styles.heading} brandStatement`}>{heading}</h2>
-          </div>
-        )}
+        <div
+          className={`${styles.textContainer} ${
+            color === 'primary' ? styles.primary : styles.secondary
+          }`}>
+          <h2 className={`${styles.heading} brandStatement`}>{heading}</h2>
+        </div>
       </div>
-      <div className={`${styles.imageBlock}`} style={{
-        backgroundImage: `url(${urlForImage(image.href)})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'none'
-      }}>
-      </div>
+      <div
+        className={`${styles.imageBlock}`}
+        style={{
+          backgroundImage: `url(${urlForImage(image.href)})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'none',
+        }}></div>
     </div>
   );
 };
