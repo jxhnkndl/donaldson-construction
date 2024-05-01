af713496d29e9a9311b5d939ed28b9f4f76111fa
jxhnkndl <jkroyston@gmail.com>
2024-05-01 16:00:15 -0400

    strip app back down to take a second swing at organizing the code

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,14 @@
 import { fetchHomePage } from '@/sanity/utils/homePageActions';
 import { fetchFeatured } from '@/sanity/utils/projectActions';
 
-import BrandStatement from '@/components/BrandStatement/BrandStatement';
-import Button from '@/components/Button/Button';
 import Carousel from '@/components/Carousel/Carousel';
-import Container from '@/components/Container/Container';
-import ProjectGrid from '@/components/ProjectGrid/ProjectGrid';
+import Heading from '@/components/Heading/Heading';
 import { IoChevronForward } from 'react-icons/io5';
-import Section from '@/components/Section/Section';
 
 const HomePage = async () => {
   const pageData = await fetchHomePage();
   const projectData = await fetchFeatured();
-  
+
   const {
     aboutCta,
     aboutHeadline,
@@ -29,50 +25,16 @@ const HomePage = async () => {
 
   return (
     <main>
-      {/* HERO CAROUSEL */}
       <Carousel images={heroImages} />
-
-      {/* ABOUT SECTION */}
-      <Section fluid={false}>
-        <Container>
-          <h1 className='heading'>{aboutHeadline}</h1>
-          <p className='bodyText'>{aboutText}</p>
-          <Button type='dark' href='/about'>
-            {aboutCta}
-            <IoChevronForward className='chevron' />
-          </Button>
-        </Container>
-      </Section>
-
-      {/* BRAND STATEMENT */}
-      <BrandStatement
-        color='primary'
-        heading={brandTopHeadline}
-        image={brandTopImage}
-        textOrder={1}
-        imageOrder={2}
-      />
-
-      {/* FEATURED PROJECTS SECTION */}
-      <Section fluid={false}>
-        <Container>
-          <h2 className='heading'>{projectsHeadline}</h2>
-          <ProjectGrid projects={projectData} />
-          <Button type='dark' href='/about'>
-            {projectsCta}
-            <IoChevronForward className='chevron' />
-          </Button>
-        </Container>
-      </Section>
-
-      {/* BRAND STATEMENT */}
-      <BrandStatement
-        color='secondary'
-        heading={brandBottomHeadline}
-        image={brandBottomImage}
-        textOrder={1}
-        imageOrder={2}
-      />
+      <Heading level={'h1'}>
+        Hello
+      </Heading>
+      <Heading level={'h2'}>
+        Hello
+      </Heading>
+      <Heading level={'h3'}>
+        Hello
+      </Heading>
     </main>
   );
 };
