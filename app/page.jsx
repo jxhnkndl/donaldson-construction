97f0ee9688549300763391a2d261043c1ce426c6
jxhnkndl <jkroyston@gmail.com>
2024-05-01 19:39:16 -0400

    add brand blocks to home page layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
 import { fetchHomePage } from '@/sanity/utils/homePageActions';
 import { fetchFeatured } from '@/sanity/utils/projectActions';
 
+import BodyText from '@/components/BodyText/BodyText';
+import BrandBlock from '@/components/BrandBlock/BrandBlock';
+import Button from '@/components/Button/Button';
 import Carousel from '@/components/Carousel/Carousel';
+import Container from '@/components/Container/Container';
 import Heading from '@/components/Heading/Heading';
-import { IoChevronForward } from 'react-icons/io5';
+import Section from '@/components/Section/Section';
+import Subheading from '@/components/Subheading/Subheading';
 
 const HomePage = async () => {
   const pageData = await fetchHomePage();
@@ -25,16 +30,45 @@ const HomePage = async () => {
 
   return (
     <main>
+      {/* HERO SECTION */}
       <Carousel images={heroImages} />
-      <Heading level={'h1'}>
-        Hello
-      </Heading>
-      <Heading level={'h2'}>
-        Hello
-      </Heading>
-      <Heading level={'h3'}>
-        Hello
-      </Heading>
+
+      {/* ABOUT SECTION */}
+      <Section fluid={false}>
+        <Container>
+          <Heading level={'h1'}>{aboutHeadline}</Heading>
+          <BodyText>{aboutText}</BodyText>
+          <Button type='dark' href='/about'>
+            {aboutCta}
+          </Button>
+        </Container>
+      </Section>
+
+      {/* BRAND BLOCK */}
+      <BrandBlock
+        color='primary'
+        text={brandTopHeadline}
+        image={brandTopImage}
+        order={1}
+      />
+
+      {/* FEATURED PROJECTS SECTION */}
+      <Section fluid={false}>
+        <Container>
+          <Heading level={'h2'}>
+            {projectsHeadline}
+          </Heading>
+        </Container>
+      </Section>
+
+      {/* BRAND BLOCK */}
+      <BrandBlock
+        color='secondary'
+        text={brandBottomHeadline}
+        image={brandBottomImage}
+        order={2}
+      />
+
     </main>
   );
 };
