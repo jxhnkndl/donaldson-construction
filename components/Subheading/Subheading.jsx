75bdd3085a58c9cdc868ca4b020af7a98108dc4f
jxhnkndl <jkroyston@gmail.com>
2024-05-02 15:52:57 -0400

    adjust subheading to act more like a heading link than a static heading

--- components/Subheading/Subheading.jsx
+++ components/Subheading/Subheading.jsx
@@ -2,10 +2,12 @@ import styles from './Subheading.module.css';
 
 const Heading = ({ level, children }) => {
   switch (level) {
-    case 'h1':
-      return <h2 className={`${styles.subheading}`}>{children}</h2>;
     case 'h2':
+      return <h2 className={`${styles.subheading}`}>{children}</h2>;
+    case 'h3':
       return <h3 className={`${styles.subheading}`}>{children}</h3>;
+    default:
+      return;
   }
 };
 
