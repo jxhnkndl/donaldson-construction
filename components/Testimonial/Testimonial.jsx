aab15868d118793a6d638a2aa7b6de3edb3134f4
jxhnkndl <jkroyston@gmail.com>
2024-05-06 15:33:43 -0400

    add animated testionials slider

--- components/Testimonial/Testimonial.jsx
+++ components/Testimonial/Testimonial.jsx
@@ -1,29 +1,56 @@
 import Image from 'next/image';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import quoteIcon from '../../assets/images/icon-quote.png';
 
 import styles from './Testimonial.module.css';
 
+const variants = {
+  initial: {
+    opacity: 0,
+    transform: 'translateX(100%)',
+  },
+  animate: {
+    opacity: 1,
+    transform: 'translateX(0%)',
+  },
+  exit: {
+    opacity: 1,
+    transform: 'translateX(-100%)',
+  },
+};
+
 const Testimonial = ({ testimonial }) => {
-  const { name, title, company, testimonialText } = testimonial;
+  const { _id, name, title, company, testimonialText } = testimonial;
 
   return (
     <div className={`${styles.container}`}>
-      <div className={`${styles.imageContainer}`}>
-        <Image
-          className={`${styles.image}`}
-          src={quoteIcon}
-          alt={'Orange quotation mark'}
-        />
-      </div>
-      <div className={`${styles.header}`}>
-        <p className={`${styles.name}`}>{name}</p>
-        <p className={`${styles.title}`}>{title}, {company}</p>
-        <div className={`${styles.divider}`}></div>
-      </div>
-      <p className={`${styles.testimonialBody}`}>
-        {testimonialText}
-      </p>
+      <AnimatePresence>
+        <motion.div
+          key={_id}
+          className={`${styles.content}`}
+          variants={variants}
+          initial='initial'
+          animate='animate'
+          exit='exit'
+          transition={{ duration: 0.5, ease: 'easeInOut' }}>
+          <div className={`${styles.imageContainer}`}>
+            <Image
+              className={`${styles.image}`}
+              src={quoteIcon}
+              alt={'Orange quotation mark'}
+            />
+          </div>
+          <div className={`${styles.header}`}>
+            <p className={`${styles.name}`}>{name}</p>
+            <p className={`${styles.title}`}>
+              {title}, {company}
+            </p>
+            <div className={`${styles.divider}`}></div>
+          </div>
+          <p className={`${styles.testimonialBody}`}>{testimonialText}</p>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
--- components/TestimonialsSlider/TestimonialsSlider.jsx
+++ components/TestimonialsSlider/TestimonialsSlider.jsx
@@ -1,15 +1,29 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
 import Testimonial from '../Testimonial/Testimonial';
 
 import styles from './TestimonialsSlider.module.css';
 
-const TestimonialsSlider = ({ testimonials }) => {
+const TestimonialsSlider = ({ testimonialData }) => {
+  const [current, setCurrent] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      current === testimonialData.length - 1
+        ? setCurrent(0)
+        : setCurrent(current + 1);
+    }, 3000);
+
+    return () => clearInterval(slideInterval);
+  }, [current]);
+
   return (
     <div className={`${styles.container}`}>
-      <div className={`${styles.track}`}>
-        {testimonials.map((testimonial) => (
-          <Testimonial key={testimonial._id} testimonial={testimonial} />
-        ))}
-      </div>
+      <Testimonial testimonial={testimonialData[current]} />
     </div>
   );
 };
