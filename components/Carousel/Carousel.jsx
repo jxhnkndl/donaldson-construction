6baecf53e6f8e2a96b9cb8fb4e5ab61c06a238e4
jxhnkndl <jkroyston@gmail.com>
2024-05-14 11:32:48 -0400

    refactor hero image carousel to perform transition animations using framer motion

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -1,15 +1,21 @@
 'use client';
 
+import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { urlForImage } from '@/sanity/lib/image';
+import { motion, AnimatePresence, easeInOut } from 'framer-motion';
 
 import styles from './Carousel.module.css';
 
 const Carousel = ({ images }) => {
   const [current, setCurrent] = useState(0);
 
+  console.log(images);
+
   useEffect(() => {
     const imageInterval = setInterval(cycleImage, 7500);
+
+    return () => clearInterval(imageInterval);
   }, [current]);
 
   const cycleImage = () => {
@@ -22,15 +28,23 @@ const Carousel = ({ images }) => {
   };
 
   return (
-    <div
-      className={`${styles.carousel}`}
-      style={{
-        backgroundImage: `url(${urlForImage(images[current].href)})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'none',
-      }}
-    />
+    <div className={`${styles.carousel}`}>
+      <AnimatePresence mode={'sync'}>
+        <motion.div
+          key={images[current].href}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5, ease: 'easeInOut' }}>
+          <Image
+            src={urlForImage(images[current].href)}
+            alt={images[current].imageAltText}
+            fill
+            objectFit='cover'
+          />
+        </motion.div>
+      </AnimatePresence>
+    </div>
   );
 };
 
