0909e46e99cb7a02d8081dbfcd40378c527ef1fb
jxhnkndl <jkroyston@gmail.com>
2024-05-20 11:48:05 -0400

    refactor hero carousel component using tailwindcss animation properties rather than framer motion

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -1,48 +1,34 @@
 'use client';
 
-import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { urlForImage } from '@/sanity/lib/image';
-import { motion, AnimatePresence, easeInOut } from 'framer-motion';
-
-import styles from './Carousel.module.css';
 
 const Carousel = ({ images }) => {
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
-    const imageInterval = setInterval(cycleImage, 7500);
+    const slideInterval = setInterval(() => {
+      if (current === images.length - 1) {
+        setCurrent(0);
+        return;
+      }
 
-    return () => clearInterval(imageInterval);
+      setCurrent(current + 1);
+    }, 5000);
+
+    return () => clearInterval(slideInterval);
   }, [current]);
 
-  const cycleImage = () => {
-    if (current === images.length - 1) {
-      setCurrent(0);
-      return;
-    }
-
-    setCurrent(current + 1);
-  };
-
   return (
-    <div className={`${styles.carousel}`}>
-      <AnimatePresence initial={false} mode={'sync'}>
-        <motion.div
-          key={images[current].href}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.5, ease: 'easeInOut' }}>
-          <Image
-            src={urlForImage(images[current].href)}
-            alt={images[current].imageAltText}
-            fill
-            objectFit='cover'
-          />
-        </motion.div>
-      </AnimatePresence>
-    </div>
+    <div
+      id='carousel'
+      className='h-[50vh] md:h-[75vh] relative transition-all duration-500'
+      style={{
+        backgroundImage: `url(${urlForImage(images[current].href)})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}></div>
   );
 };
 
