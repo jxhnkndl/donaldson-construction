770074f3106bc241bffdb88b722794c8d1c73246
jxhnkndl <jkroyston@gmail.com>
2024-05-20 15:50:24 -0400

    refactor class list

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,20 +1,15 @@
 const Heading = ({ level, children }) => {
   switch (level) {
     case 'h1':
-      return (
-        <h1 className='mb-4 text-[2rem] md:text-[2.75rem] lg:text-[4rem] leading-[2.5rem] md:leading-[3.25rem] lg:leading-[4.5rem] font-medium'>
-          {children}
-        </h1>
-      );
+      return <h1 className={heading}>{children}</h1>;
     case 'h2':
-      return (
-        <h2 className='mb-4 text-[2rem] md:text-[2.75rem] lg:text-[4rem] leading-[2.5rem] md:leading-[3.25rem] lg:leading-[4.5rem] font-medium'>
-          {children}
-        </h2>
-      );
+      return <h2 className={heading}>{children}</h2>;
     default:
       return;
   }
 };
+
+const heading =
+  'mb-4 text-[2rem] md:text-[2.75rem] lg:text-[4rem] leading-[2.5rem] md:leading-[3.25rem] lg:leading-[4.5rem] font-heading font-medium';
 
 export default Heading;
