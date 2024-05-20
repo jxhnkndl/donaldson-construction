99426f6c8ef689d1dd2f1ab0ae7d7d770bea8620
jxhnkndl <jkroyston@gmail.com>
2024-05-20 12:52:41 -0400

    adjust font weight

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -2,13 +2,13 @@ const Heading = ({ level, children }) => {
   switch (level) {
     case 'h1':
       return (
-        <h1 className='mb-4 text-[2rem] md:text-[2.75rem] lg:text-[4rem] leading-[2.5rem] md:leading-[3.25rem] lg:leading-[4.5rem] font-fw-heading'>
+        <h1 className='mb-4 text-[2rem] md:text-[2.75rem] lg:text-[4rem] leading-[2.5rem] md:leading-[3.25rem] lg:leading-[4.5rem] font-medium'>
           {children}
         </h1>
       );
     case 'h2':
       return (
-        <h2 className='mb-4 text-[2rem] md:text-[2.75rem] lg:text-[4rem] leading-[2.5rem] md:leading-[3.25rem] lg:leading-[4.5rem] font-fw-heading'>
+        <h2 className='mb-4 text-[2rem] md:text-[2.75rem] lg:text-[4rem] leading-[2.5rem] md:leading-[3.25rem] lg:leading-[4.5rem] font-medium'>
           {children}
         </h2>
       );
