13f0aac35e8d9c567aea71a614a4bb349b1f540b
jxhnkndl <jkroyston@gmail.com>
2024-05-21 15:35:34 -0400

    add spacing prop to push headings a little further away from non-text components below them

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,15 +1,22 @@
-const Heading = ({ level, children }) => {
+const Heading = ({ level, spacing, children }) => {
   switch (level) {
     case 'h1':
-      return <h1 className={heading}>{children}</h1>;
+      return (
+        <h1 className={`${heading} ${spacing === 'sm' ? 'mb-4' : 'mb-8'}`}>
+          {children}
+        </h1>
+      );
     case 'h2':
-      return <h2 className={heading}>{children}</h2>;
+      return (
+        <h2 className={`${heading} ${spacing === 'sm' ? 'mb-4' : 'mb-8'}`}>
+          {children}
+        </h2>
+      );
     default:
       return;
   }
 };
 
-const heading =
-  'mb-4 text-[2rem] md:text-[2.75rem] lg:text-[4rem] leading-[2.5rem] md:leading-[3.25rem] lg:leading-[4.5rem] font-heading font-medium';
+const heading = `text-[2rem] md:text-[2.75rem] lg:text-[4rem] leading-[2.5rem] md:leading-[3.25rem] lg:leading-[4.5rem] font-heading font-medium`;
 
 export default Heading;
